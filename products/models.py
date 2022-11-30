3d9efac057bb5597fb692369f6d29e5227b4d2af
yair-go <irigo1@gmail.com>
2022-11-30 11:32:38 +0200

    create model Order

--- products/models.py
+++ products/models.py
@@ -6,3 +6,13 @@ class Product(models.Model):
     price = models.FloatField()
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)
+
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=255)
+    customer_email = models.CharField(max_length=255)
+    customer_address = models.CharField(max_length=255)
+    order_date = models.DateTimeField()
+    shipping_date = models.DateTimeField()
+    delivery_date = models.DateTimeField()
+
